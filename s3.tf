f005cbf5edd54645e4550bd199eb9814200f968e
bassam <bassamkhamis7@gmail.com>
2023-01-17 11:23:45 -0500

    s3 file change

--- s3.tf
+++ s3.tf
@@ -1,8 +1,4 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "a4l-pipeline-artifacts"
-}
-
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.codepipeline_artifacts.id
   acl    = "private"
 }
